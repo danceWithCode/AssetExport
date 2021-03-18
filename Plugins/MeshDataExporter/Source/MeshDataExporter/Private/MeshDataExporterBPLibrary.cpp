b38533c448e56ec6532de95749f7d598228d0ec7
jinannan <372422820@qq.com>
2021-03-18 14:31:43 +0800

    1.add MeshDataExporter plugin 2.add editor utility bp

---
+++ Plugins/MeshDataExporter/Source/MeshDataExporter/Private/MeshDataExporterBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MeshDataExporterBPLibrary.h"
+#include "MeshDataExporter.h"
+
+UMeshDataExporterBPLibrary::UMeshDataExporterBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UMeshDataExporterBPLibrary::MeshDataExporterSampleFunction(float Param)
+{
+	return -1;
+}
+
